e84cce88c0076e1697651f550a04ded44f6e26d7
DarkDampSquib <andrin.geiger1998@gmail.com>
2019-10-30 18:32:55 +0100

    cepheus: Enable FOD

--- device.mk
+++ device.mk
@@ -32,9 +32,6 @@ $(call inherit-product, device/xiaomi/sm8150-common/sm8150.mk)
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_sm8150
 
-PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
