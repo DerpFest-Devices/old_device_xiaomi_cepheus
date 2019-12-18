33120eda2709617c1a76d95f450eecf3cdd59484
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 15:33:18 +0000

    cepheus: Include customized version of ANX Camera
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- aosip_cepheus.mk
+++ aosip_cepheus.mk
@@ -29,3 +29,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="cepheus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/main.mk)
